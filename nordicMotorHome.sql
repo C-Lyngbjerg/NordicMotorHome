c7152fabe0dea2613691b7ae87d6b574a27010fb
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2020-05-22 10:26:48 +0200

    Update nordicMotorHome.sql

--- nordicMotorHome.sql
+++ nordicMotorHome.sql
@@ -9,7 +9,8 @@ DROP TABLE IF EXISTS zips;
 CREATE TABLE zips
 (
 	zip_code INT PRIMARY KEY UNIQUE NOT NULL,
-  zip_city VARCHAR(45) NOT NULL
+    zip_city VARCHAR(45) NOT NULL
+
 );
 
 DROP TABLE IF EXISTS customers;
@@ -44,7 +45,8 @@ DROP TABLE IF EXISTS motorhomes;
 
 CREATE TABLE motorhomes
 (
-	motorhome_reg_number VARCHAR(45) UNIQUE PRIMARY KEY NOT NULL,
+	motorhome_id INT UNIQUE PRIMARY KEY NOT NULL,
+	motorhome_reg_number VARCHAR(45) NOT NULL,
     motorhome_brand VARCHAR(45) NOT NULL,
     motorhome_room_height INT NOT NULL,
     motorhome_model VARCHAR(45) NOT NULL,
@@ -52,6 +54,18 @@ CREATE TABLE motorhomes
     type_id INT NOT NULL,
     CONSTRAINT motorhomes_fk_mh_types
 		FOREIGN KEY (type_id) REFERENCES mh_types (type_id)
+);
+
+DROP TABLE IF EXISTS repairs;
+
+CREATE TABLE repairs
+(
+repair_id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+repair_description VARCHAR(45) NOT NULL,
+repair_date DATE NOT NULL,
+motorhome_id INT NOT NULL,
+CONSTRAINT repairs_fk_motorhomes
+	FOREIGN KEY (motorhome_id) REFERENCES motorhomes (motorhome_id)
 );
 
 DROP TABLE IF EXISTS contracts;
@@ -64,16 +78,15 @@ CREATE TABLE contracts
     contract_end_date DATE NOT NULL,
     contract_odometer_start INT NOT NULL,
     contract_extra_bike_rack TINYINT NOT NULL,
-    contract_extra_bed_sheets TINYINT NOT NULL,
     contract_extra_child_seat TINYINT NOT NULL,
     contract_extra_picnic_table TINYINT NOT NULL,
     contract_extra_chairs TINYINT NOT NULL,
     customer_id INT NOT NULL,
-    motorhome_reg_number VARCHAR(45) NOT NULL,
+    motorhome_id INT NOT NULL,
     CONSTRAINT contracts_fk_customers
 		FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
 	CONSTRAINT contracts_fk_motorhomes
-		FOREIGN KEY (motorhome_reg_number) REFERENCES motorhomes (motorhome_reg_number)
+		FOREIGN KEY (motorhome_id) REFERENCES motorhomes (motorhome_id)
 );
 
 DROP TABLE IF EXISTS invoices;
@@ -84,35 +97,13 @@ CREATE TABLE invoices
     invoice_total_price DECIMAL NOT NULL,
     invoice_distance_driven INT NOT NULL,
     invoice_odometer_end INT NOT NULL,
+    invoice_rent_days INT NOT NULL,
     contract_id INT NOT NULL,
+    invoice_fuel_gage BOOLEAN NOT NULL,
     CONSTRAINT invoices_fk_contracts
 		FOREIGN KEY (contract_id) REFERENCES contracts (contract_id)
 );
 
-INSERT INTO zips VALUES
-(2700, 'Brønshøj'),
-(2200, 'Nørrebro'),
-(9800,'Hjørring'),
-(4850,'Stubbekøbing'),
-(1416, 'Christianshavn');
-
-INSERT INTO mh_types VALUES
-(1, 'compact', 1999, 2, 2),
-(2, 'Medium', 2499, 3, 3);
-
-INSERT INTO customers VALUES
-(1, 'Christoffer', 'Bjerge', 'Valkyriegade', 'abc1234', 'B','60110716', 'Dansk', 2200),
-(2, 'William', 'Omoe', 'Lersoe Park allé', '123abc', 'E', '12345678', 'Dansk', 4850);
-
-INSERT INTO motorhomes VALUES
-('Reg12345', 'Volkswagen', '180', 'AMG', 1, 1),
-('AM1235', 'Pegueot', '200', 'Cayenne', 200, 2);
-
-INSERT INTO contracts VALUES
-(1, 9999, '2020-01-01', '2020-01-02', 1, 1, 0, 1, 0, 1,'Reg12345'),
-(2, 5555, '2020-02-02', '2020-02-03', 1, 0, 1, 0, 1, 1,'AM1235');
-
-
 
 INSERT INTO zips VALUES
 (2700, 'Brønshøj'),
@@ -130,16 +121,19 @@ INSERT INTO customers VALUES
 (2, 'William', 'Omoe', 'Lersoe Park allé', '123abc', 'E', '12345678', 'Dansk', 4850);
 
 INSERT INTO motorhomes VALUES
-('Reg12345', 'Volkswagen', '180', 'AMG', 1, 1),
-('AM1235', 'Pegueot', '200', 'Cayenne', 200, 2);
+(1, 'Reg12345', 'Volkswagen', '180', 'AMG', 1, 1),
+(2, 'AM1235', 'Pegueot', '200', 'Cayenne', 200, 2);
+
+INSERT INTO repairs VALUES
+(1, 'shits fucked yo', '2020-02-22', 1);
 
 INSERT INTO contracts VALUES
-(1, 9999, '2020-01-01', '2020-01-02', 1, 1, 0, 1, 0, 1,'Reg12345'),
-(2, 5555, '2020-02-02', '2020-02-03', 1, 0, 1, 0, 1, 1,'AM1235');
+(1, 9999, '2020-01-01', '2020-01-02', 1, 1, 0, 1, 0, 1,'1'),
+(2, 5555, '2020-02-02', '2020-02-03', 1, 0, 1, 0, 1, 1,'2');
 
 INSERT INTO invoices VALUES
-(1,11,0,500,1),
-(2,11,0,200,2);
+(1,0,0,500,0,1,0);
+-- (2,0,0,200,0,2,0);
 
 SELECT inv.invoice_odometer_end - con.contract_odometer_start AS total_driven
 FROM contracts con JOIN invoices inv ON inv.contract_id = con.contract_id;
